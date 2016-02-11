a9c28368ec01195b35e5994131da8fd97289c142
Levi Bostian <levibostian@levis-macbook-pro.local>
2016-02-11 12:37:01 -0600

    Initial Commit

---
+++ LaunchAppFromURL-iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LaunchAppFromURL-iOS
+//
+//  Created by Levi Bostian on 2/11/16.
+//  Copyright © 2016 Curiosity IO. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
